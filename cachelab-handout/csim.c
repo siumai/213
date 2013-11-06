37b5f1c59c5c89ab75f7684244013c4dd8dccfd8
siumai <siumaicat@gmail.com>
2013-11-05 23:35:03 -0800

    direct mapped cache working

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -5,19 +5,24 @@
 
 struct line {
     int valid;
-    long tag;
+    unsigned long tag;
 };
 
 typedef struct line LINE;
 
 int main()
 {
-    //printf("%lu", sizeof(unsigned long long));
+    /*unsigned int test = 0x80000000;
+    test = test >> 4;
+    printf("shift %x\n", test);*/
     
-    int s, E, sets;
+    int s, E, b, sets;
     s = 1;
     E = 1;
+    b = 1;
     sets = pow(2, s);
+    int tagShift = b + s;
+    unsigned long setIndexMask = (sets-1)<<b;
 
     LINE** cache;
     cache = (LINE**) malloc(sets*sizeof(LINE*));
@@ -40,10 +45,51 @@ int main()
     }
 
     char instruction, cm;
-    long address;
+    unsigned long address;
     int rsize;
+    int hit = 0, miss = 0, eviction = 0;
+
     while(4 == fscanf(ifp, " %c %lx %c %d", &instruction, &address, &cm, &rsize)) {
 	printf("%c \t %lx \t %d\n", instruction, address, rsize);
+	
+	int setIndex = (address & setIndexMask)>>b;
+	printf("set index: %x\n", setIndex);
+	unsigned long tg = address >> tagShift;
+	printf("tag: %lx\n", tg);
+	LINE *lnp = &(cache[setIndex][0]);
+	if((*lnp).valid) {
+	    printf("cache tag: %lx\n", (*lnp).tag);
+	}
+	if(instruction=='L' || instruction=='S') {
+	    if((*lnp).valid==1 && tg == (*lnp).tag) {
+		hit++;
+		puts("hit");
+	    } else {
+		miss++;
+		if((*lnp).valid==1) {
+		    puts("e");
+		    eviction++;
+		}
+		(*lnp).valid = 1;
+		(*lnp).tag = tg;
+		puts("miss");
+	    }
+	} else if(instruction=='M') {
+	    if((*lnp).valid==1 && tg == (*lnp).tag) {
+		hit += 2;
+		puts("hit*2");
+	    } else {
+		hit++;
+		miss++;
+		if((*lnp).valid == 1) {
+		    puts("e");
+		    eviction++;
+		}
+		(*lnp).valid = 1;
+		(*lnp).tag = tg;
+		puts("hit miss");
+	    }	
+	}
     }
 
     if(feof(ifp)) {
@@ -56,6 +102,6 @@ int main()
     }
     free(cache);
 
-    printSummary(0, 0, 0);
+    printSummary(hit, miss, eviction);
     return 0;
 }
